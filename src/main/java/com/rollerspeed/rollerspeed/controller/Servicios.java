bf7e2f277a2f82ba6173264b0ccff461c5018a4b
lgiraldo-dev <lgiraldo@quanspire.com>
2026-03-05 21:22:43 -0500

    Modifico indentación del controller servicios

--- src/main/java/com/rollerspeed/rollerspeed/controller/Servicios.java
+++ src/main/java/com/rollerspeed/rollerspeed/controller/Servicios.java
@@ -1,19 +1,15 @@
-package com.rollerspeed.rollerspeed.Controller;
+package com.rollerspeed.rollerspeed.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @Controller
 @RequestMapping("/servicios")
 public class Servicios {
 
-@GetMapping
-    public String Servicios() {
-        
+    @GetMapping
+    public String servicios() {
         return "servicios"; 
     }
 }
