25676d97ba1bbb13cf0cdddd871aa2af57ba0119
CriVasan <cristian.vargas@est.iudigital.edu.co>
2026-03-21 17:08:29 -0500

    Agrega pagina user con estilo Roller Speed y ruta /user

--- src/main/java/com/rollerspeed/rollerspeed/controller/UserController.java
+++ src/main/java/com/rollerspeed/rollerspeed/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.rollerspeed.rollerspeed.controller;
 
+import java.util.Collections;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+	@GetMapping("/user")
+	public String userPage(Model model) {
+		model.addAttribute("datosUsuarios", Collections.emptyList());
+		return "user";
+	}
 }
