fe22d9bbae6e3b04c7333f8399f42cb96444a14a
Lancero741 <segundaguerram74@gmail.com>
2026-03-21 15:02:45 -0500

    Actualizar UserController y users.html: agregar @RequestMapping, cambiar ruta a /list, corregir método getAllUsers, y completar template con tabla Bootstrap

--- src/main/java/com/rollerspeed/rollerspeed/controller/UserController.java
+++ src/main/java/com/rollerspeed/rollerspeed/controller/UserController.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rollerspeed.rollerspeed.Service.UserService;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping("/list")
     public String getAllUsers(Model model) {
-        model.addAttribute("datosUsuarios", userService.getAllusers());
+        model.addAttribute("datosUsuarios", userService.getAllUsers());
         return "users";
     }   
 }
