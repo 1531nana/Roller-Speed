6d43264ba021aa2018ec7500cee9b17e380b25b8
Lancero741 <segundaguerram74@gmail.com>
2026-03-21 14:47:18 -0500

    Corregir UserModel.java: orden correcto de package e imports, y formato de anotaciones

--- src/main/java/com/rollerspeed/rollerspeed/Model/UserModel.java
+++ src/main/java/com/rollerspeed/rollerspeed/Model/UserModel.java
@@ -1,30 +1,29 @@
-import jakarta.persistence.*;
-import lombok.*;
-
 package com.rollerspeed.rollerspeed.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tbl_users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_email", length = 100,nullable = false, unique = true)
+    @Column(name = "user_email", length = 100, nullable = false, unique = true)
     private String email;
 
-    @Column(name = "user_password", length = 20,nullable = false)
+    @Column(name = "user_password", length = 20, nullable = false)
     private String password;
-
 
     @Column(name = "user_name", length = 30, nullable = false, unique = true)
     private String username;
+
+}
 
     
     
