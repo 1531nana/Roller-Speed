8b34c6991264966a419cb8626fcb585f148f1ef0
Lancero741 <segundaguerram74@gmail.com>
2026-03-21 14:32:46 -0500

    Agregar lógica de usuarios: UserService con getAllusers, UserController con endpoint GET, y template users.html

--- src/main/java/com/rollerspeed/rollerspeed/Service/UserService.java
+++ src/main/java/com/rollerspeed/rollerspeed/Service/UserService.java
@@ -1,4 +1,24 @@
 package com.rollerspeed.rollerspeed.Service;
 
+import java.util.List;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties.Apiversion.Use;
+
+import com.rollerspeed.rollerspeed.Model.UserModel;
+import com.rollerspeed.rollerspeed.Repository.UserRepository;
+
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserModel> getAllusers() {
+        return userRepository.findAll();
+        
+    }
 }
--- src/main/java/com/rollerspeed/rollerspeed/controller/UserController.java
+++ src/main/java/com/rollerspeed/rollerspeed/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.rollerspeed.rollerspeed.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String getAllUsers() {
+        return "users";
+    }   
 }
