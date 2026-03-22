520d7c4678970010101f36fbfb647d66d2c6a81a
lgiraldo-dev <lgiraldo@quanspire.com>
2026-03-21 21:52:01 -0500

    Se agrega la funcionalidad para agregar un usuario en BD

--- src/main/java/com/rollerspeed/rollerspeed/Repository/UserRepository.java
+++ src/main/java/com/rollerspeed/rollerspeed/Repository/UserRepository.java
@@ -3,9 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.rollerspeed.rollerspeed.Model.UserModel;
 
-
+@Repository
 public interface UserRepository  extends JpaRepository<UserModel, Long>{
 
-    
 } 
-
