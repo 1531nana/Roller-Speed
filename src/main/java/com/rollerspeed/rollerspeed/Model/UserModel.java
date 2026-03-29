8b933016f58fb574acd03c401bc33e76aca396f6
lgiraldo-dev <lgiraldo@quanspire.com>
2026-03-28 22:21:54 -0500

    Se agrega la estrucutra base para openAPI

--- src/main/java/com/rollerspeed/rollerspeed/Model/UserModel.java
+++ src/main/java/com/rollerspeed/rollerspeed/Model/UserModel.java
@@ -1,8 +1,10 @@
 package com.rollerspeed.rollerspeed.Model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Schema(description = "Representa a un usuario registrado en Roller Speed")
 @Entity
 @Table(name = "tbl_users")
 @Data
@@ -10,16 +12,20 @@ import lombok.*;
 @AllArgsConstructor
 public class UserModel {
 
+    @Schema(description = "ID unico del usuario", example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(description = "Nombre de usuario unico", example = "juan_patinador")
     @Column(name = "user_name", length = 40, nullable = false, unique = true)
     private String username;
 
+    @Schema(description = "Correo electronico del usuario", example = "juan@rollerspeed.com")
     @Column(name = "user_email", length = 100, nullable = false, unique = true)
     private String email;
 
+    @Schema(description = "Contrasena del usuario", example = "pass123")
     @Column(name = "user_password", length = 20, nullable = false)
     private String password;
 
---
+++ src/main/java/com/rollerspeed/rollerspeed/controller/UserRestController.java
@@ -0,0 +1,21 @@
+package com.rollerspeed.rollerspeed.controller;
+
+import com.rollerspeed.rollerspeed.Model.UserModel;
+import com.rollerspeed.rollerspeed.Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Gestion de Usuarios", description = "API para crear, consultar, actualizar y eliminar usuarios de Roller Speed")
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+}
